5144a5f6bc87964b1cc148449614692b798628a3
thibaultbarbie <thibault.barbie@free.fr>
2016-04-22 16:16:53 +0900

    Added a cycle to make the hand moving a little bit of time.

--- script.py
+++ script.py
@@ -5,22 +5,25 @@ b=DHand()
 b.alarm_reset()
 b.modbus_on()
 b.servo_on()
-b.home_return()
-b.start_off()
-b.start_on()
+#b.home_return()
+#b.start_off()
+#b.start_on()
 
-time.sleep(0.500)
+time.sleep(0.5)
 
-b.move_pos1()
-b.start_off()
-b.start_on()
+i=0
+while i<10:
+    if(i%2==0):
+        b.move_pos1()
+    else:
+        b.move_pos2()
+    b.start_off()
+    b.start_on()
 
-print "Sleep"
-time.sleep(3)
-print "End of sleep"
-
-b.move_pos2()
-b.start_off()
-b.start_on()
+    print "Sleep"
+    time.sleep(1)
+    print "End of sleep"
+        
+    i=i+1
 
 b.pause()
