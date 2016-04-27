d1d190f2b5e1d39f5df3656b9df124a33795f013
thibaultbarbie <thibault.barbie@free.fr>
2016-04-27 12:28:50 +0900

    Removed the 'move pos' methods.

--- driver.py
+++ driver.py
@@ -42,21 +42,6 @@ class DHand:
         self.s.write_bit(register,0,functioncode=5)
         self.s.write_bit(register,1,functioncode=5)
         
-    def move_pos1(self):
-        # Move to position 1
-        register=int('9800',16)
-        self.s.write_register(register,0001,functioncode=6)
-
-    def move_pos2(self):
-        # Move to position 2
-        register=int('9800',16)
-        self.s.write_register(register,0002,functioncode=6)
-
-    def move_pos3(self):
-        # Move to position 3
-        register=int('9800',16)
-        self.s.write_register(register,3,functioncode=6)
-
     def start_off(self):
         # Turn OFF the start signal
         register=int('0D00',16)
