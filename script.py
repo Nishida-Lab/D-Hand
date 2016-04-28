1dad6fc28995b03d6b37bfa2d35e548bb871884f
thibaultbarbie <thibault.barbie@free.fr>
2016-04-28 10:55:39 +0900

    Changed the script to adapt to the driver changes.

--- script.py
+++ script.py
@@ -3,39 +3,40 @@
 from driver import *
 import time
 
-b=DHand()
+b=Servo("ttyUSB0",False)
 b.alarm_reset()
 
 b.modbus_on()
 b.servo_on()
 
 # Should have a sleep if not it does not work
-time.sleep(0.5)
+time.sleep(1.5)
 
 i=0
 while i<4:
     if(i%2==0):
-        b.move_absolute_position(0.1,30,0.1,0.5)
+        b.move_absolute_position(0.1,20,0.1,0.2)
     else:
-        b.move_absolute_position(13,30,0.1,0.5)
+        b.move_absolute_position(13,20,0.1,0.2)
 
     time.sleep(0.7)
-    b.read_position()    
+    b.read_torque()    
     i=i+1
 
 
-while i<8:
+while i<10:
     if(i%2==0):
-        b.move_absolute_position(0.1,15,0.1,0.5)
+        b.move_absolute_position(0.1,10,0.1,0.2)
     else:
-        b.move_absolute_position(13,15,0.1,0.5)
+        b.move_absolute_position(13,10,0.1,0.2)
 
     time.sleep(1.2)
-    b.read_position()    
+    b.read_torque()    
     i=i+1
 
+print "Home return"
+b.home_return()
+time.sleep(3)
 
-time.sleep(1)
-
-b.pause()
+b.servo_off()
 
